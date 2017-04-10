9207d33f17c092e79e3a23148cbd43044de71a06
WU <accept441@gmail.com>
2017-04-10 11:52:32 +0800

    add other test

--- holow/Form1.cs
+++ holow/Form1.cs
@@ -26,5 +26,10 @@ namespace holow
         {
             label1.Text = "hollow word";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text; 
+        }
     }
 }
