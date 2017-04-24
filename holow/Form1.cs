07953d32bc1172f04d7a1d66cbb46e997ad05213
ray <ray.880922@gmail.com>
2017-04-24 11:13:46 +0800

    add btn3

--- holow/Form1.cs
+++ holow/Form1.cs
@@ -34,7 +34,7 @@ namespace holow
 
         private void button3_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hate U");
+            MessageBox.Show("hate the world～～");
         }
     }
 }
